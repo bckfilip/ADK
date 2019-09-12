9b1c160efd7a05b23bbfe9082b8491858952d07e
Filip Bäck <45845878+bckfilip@users.noreply.github.com>
2019-09-12 17:47:12 +0200

    Update kladd.java

--- kladd.java
+++ kladd.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.io.*;
 import java.lang.StringBuilder;
 
+
 public class kladd {
     /*
     index is sorted list with each word + the first position of said word in ordlista(LMH points here)
@@ -11,46 +12,53 @@ public class kladd {
     
     public static void main(String[] args){
         try{    
-            File file = new File("/mnt/c/Users/filip/Desktop/ADKLab1/testing.txt");
-            File write = new File("/mnt/c/Users/filip/Desktop/ADKLab1/tom.txt");
-            FileWriter fw = new FileWriter(write);
-            ArrayList<String> index = new ArrayList<String>();
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            //BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("testing.txt"), "ISO-8859-1"));
+            File read = new File("tokenized.txt"); // skapar filen med ord och alla förekomster(sorterade filen från tokenized <- korpus )
+            File write = new File("index_file.txt"); // Skapar filen som vi skriver den berabetade read till
+            FileWriter fw = new FileWriter(write);  // objekt för att skriva till index_file
+            ArrayList<String> index = new ArrayList<String>(); // Lista för att apppenda alla förekomster av ordens position
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(read), "ISO-8859-1")); // Bufferedreader för att läsa från tokenized.txt 
             StringBuilder builder = new StringBuilder();
             String oldWord = ""; 
             String currentWord;
-            //String[] wordAndPos = {"",""};
+            int occurences = 0;
+            
 
             while((currentWord = br.readLine()) != null){ 
                 String[] wordAndPos = currentWord.split(" ");
-                //System.out.println(wordAndPos[0]+ " hgghgh");
-                if(oldWord == ""){
+                if(oldWord == ""){      // true första gången while körs används enbart för att behandla första ordet (a)   
                     builder.append(wordAndPos[0]);
                     builder.append(",");
                     builder.append(wordAndPos[1]);
                     oldWord = wordAndPos[0];
+                    occurences = 1;
                 }else{
-                     if(wordAndPos[0].equals(oldWord)){
-                        builder.append(",");
-                        builder.append(wordAndPos[1]);
+                    if(wordAndPos[0].equals(oldWord)){ // om man läser samma ord som tidigare appenda nya förekomsten till String buildern
+                    builder.append(",");
+                    builder.append(wordAndPos[1]);
+                    occurences++;
                     }else{
-                        index.add(builder.toString());
-                        builder = new StringBuilder();
-                        
-                        builder.append(wordAndPos[0]);
-                        builder.append(",");
-                        builder.append(wordAndPos[1]);
-                        oldWord = wordAndPos[0];
+
+                    
+                    builder.insert(0,occurences); // insertar #förekomster i början av strängen
+                    int length = String.valueOf(occurences).length(); //math
+                    builder.insert(length,",");
+                    occurences = 1;
+                    index.add(builder.toString()); // om ett nytt ord dyker upp lägg till ordet och förekomster i listan med ord fölt av förekomst a,bytepos,bytepos
+                    
+                    builder = new StringBuilder();  // ny string builder(fungerar som flush)
+                    builder.append(wordAndPos[0]); //lägg till nytt ord
+                    builder.append(",");          
+                    builder.append(wordAndPos[1]);  //första bytepos
+                    oldWord = wordAndPos[0];
                     }
                 }
-
             }
-           
-            br.close();
+            index.add(builder.toString());      //lägger till sista ordet
+            br.close();                         //stänger buffered reader
 
-            for(int i = 0; i < index.size(); i++){ // i <= index.size()
-                fw.write(index.get(i) + "\n"); 
+            //Skriver resultat till index_file
+            for(int i = 0; i < index.size(); i++){
+                fw.write((index.get(i) + "\n")); 
                 fw.flush();   
             }
             fw.close();
