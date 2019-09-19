9d093d86b0b292a0b3c71a7af508c671cac9c7c4
filip <filip@ubuntu.ubuntu-domain>
2019-09-19 23:09:25 +0200

    positionNext increment in the first whileloop inside index.java was placed wrong. Counted bytelength of the word in the question. Only thing left to do should be to figure out the encoding. One part of the program counts å,ä,ö as two bytes while other parts only counts them as one. I am confused.

--- index.java
+++ index.java
@@ -8,45 +8,43 @@ public class index{
     public static void main(String[] args) throws IOException{
         long startTime = System.currentTimeMillis();
         try{
-            
-            File tokenized = new File("/var/tmp/tokenized.txt");
-            File indexFile = new File("/var/tmp/index.txt");
+
+            File tokenized = new File("tokenized");
+            File indexFile = new File("index.txt");
             BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(tokenized), "ISO-8859-1"));
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(indexFile), "ISO-8859-1"));
 
-            String[] currentLine = reader.readLine().split(" ");
+            String firstLine = reader.readLine();
+            String[] currentLine = firstLine.split(" ");
             String nextLine = reader.readLine();
             int occurences = 0;
             int positionFirst = 0;
-            int positionNext = 0;
+            int positionNext = firstLine.getBytes("ISO-8859-1").length +1;
 
-            
-            
+
             while(nextLine != null){
                 String currentWord = currentLine[0];
                 String[] newLineWords = nextLine.split(" ");
                 String nextWord = newLineWords[0];
-             
-                positionNext += nextLine.getBytes("ISO-8859-1").length + 1;
-                occurences++;
-
 
                 if(!currentWord.equals(nextWord)){
                     writer.write(currentWord + "," + occurences + "," + positionFirst + "\n");
                     occurences = 0;
                     positionFirst = positionNext;
                 }
+                positionNext += nextLine.getBytes("ISO-8859-1").length + 1;
+                occurences++;
                 currentLine = newLineWords;
                 nextLine = reader.readLine();
             }
             occurences++;
             String currentWord = currentLine[0];
             writer.write(currentWord + "," + occurences + "," + positionFirst + "\n");
-            
+
             reader.close();
             writer.close();
 
-            
+
 
         }catch(IOException e){
             e.printStackTrace();
@@ -56,7 +54,7 @@ public class index{
         long stopTime = System.currentTimeMillis();
         long elapsedTime = stopTime - startTime;
         System.out.println(elapsedTime);
-     
+
     }
     public static void createHash() throws IOException{
 
@@ -65,13 +63,13 @@ public class index{
             hashList.add(0);
         }
 
-        File indexFile = new File("/var/tmp/index.txt");
-        File hashFile = new File("/var/tmp/hash.txt");
+        File indexFile = new File("index.txt");
+        File hashFile = new File("hash.txt");
         RandomAccessFile index = new RandomAccessFile(indexFile, "r");
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(hashFile), "ISO-8859-1"));
         int pos = 0;
         String currentLine;
-      
+
         while((currentLine = index.readLine()) != null){
             String currentWord = currentLine.split(",")[0];
             int hashCurrent = getHash(currentWord);
@@ -87,7 +85,7 @@ public class index{
         writer.close();
         index.close();
     }
-        
+
     private static int charValue(char chr) {
         if (chr == 229) { // 'a
             return 28;
@@ -104,7 +102,7 @@ public class index{
     }
 
     public static int getHash(String word){
-       
+
         int hashIndex = 0;
         for (int l = 0; l < Math.min(3, word.length()); l++) {
             hashIndex += charValue(word.charAt(l)) * Math.pow(30, (2 - l));
