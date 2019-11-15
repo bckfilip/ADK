ad0a0c71167a7db4779fc264aef63f031c71c27a
Filip Back <filbac@kth.se>
2019-11-15 15:36:38 +0100

    heheh

--- lab3/MaxFlow.java
+++ lab3/MaxFlow.java
@@ -6,14 +6,21 @@ import java.io.FileNotFoundException;
 public class MaxFlow {
 
   public class Edge {
-    int granne;
+
+    int from;
+    int to;
     int capacity;
     int flow;
+    Edge reverseEdge;
+    Boolean direction; //true::=forward
 
-    public Edge(int granne, int capacity) {
-      this.granne = granne;
+    public Edge(int from, int to, int capacity){
+      this.from = from;
+      this.to = to;
       this.capacity = capacity;
       this.flow = 0;
+      this.reverseEdge = null;
+      this.direction = null;
     }
 
     public int restCapacity() {
@@ -27,92 +34,109 @@ public class MaxFlow {
     MaxFlow mf = new MaxFlow();
     Kattio io;
     io = new Kattio(System.in, System.out);
-    HashMap<Integer, ArrayList<Edge>> grannMap = new HashMap<Integer, ArrayList<Edge>>(); // kan gå sanbbare att ge fixed sized
+    // se om ge fixed size till map.
+    HashMap<Integer, ArrayList<Edge>> grannMap = new HashMap<Integer, ArrayList<Edge>>();
     HashMap<Integer, ArrayList<Edge>> residualGraf = new HashMap<Integer, ArrayList<Edge>>();
     System.out.println("before input");
-    // läser input
+    /*
+     * skapar två identiska grafer som inehpller kanter och bakåtkanter.
+     */
     int v = io.getInt();
     int s = io.getInt();
     int t = io.getInt();
     int e = io.getInt();
     for (int i = 0; i < e; i++) {
       System.out.println("creating maps");
-      int a = io.getInt();
-      int b = io.getInt();
+      int from = io.getInt();
+      int to = io.getInt();
       int capacity = io.getInt();
-      // a -> b
-      if (!(grannMap.containsKey(a))) {
-        System.out.println("first if");
-        ArrayList<Edge> value = new ArrayList<Edge>();
-        grannMap.put(a, value);
-        residualGraf.put(a, value);
-        Edge tmpEdge = mf.new Edge(b, capacity);
-        grannMap.get(a).add(tmpEdge);
-        residualGraf.get(a).add(tmpEdge);
-      } // b -> a
-      if (!(grannMap.containsKey(b))) {
-        System.out.println("second if");
-        ArrayList<Edge> value = new ArrayList<Edge>();
-        grannMap.put(b, value);
-        residualGraf.put(b, value);
-        Edge tmpEdge = mf.new Edge(a, capacity);
-        grannMap.get(b).add(tmpEdge);
-        residualGraf.get(b).add(tmpEdge);
+      Edge forward = mf.new Edge(from, to, capacity);
+      Edge backward = mf.new Edge(to, from, capacity);
+      backward.reverseEdge = forward;
+      backward.direction = false;
+      forward.reverseEdge = backward;
+      forward.direction = true;
+
+      if (!grannMap.containsKey(from)) {
+        ArrayList<Edge> edgeFrom = new ArrayList<Edge>();
+        ArrayList<Edge> edgeFromR = new ArrayList<Edge>();
+        edgeFrom.add(forward);
+        edgeFromR.add(forward);
+        grannMap.put(from, edgeFrom);
+        residualGraf.put(from, edgeFromR);
+        if (!grannMap.containsKey(to)) {
+          ArrayList<Edge> edgeTo = new ArrayList<Edge>();
+          ArrayList<Edge> edgeToR = new ArrayList<Edge>();
+          edgeTo.add(backward);
+          edgeToR.add(backward);
+          grannMap.put(to, edgeTo);
+          residualGraf.put(to, edgeToR);
+        } else {
+          grannMap.get(to).add(backward);
+          residualGraf.get(to).add(backward);
+        }
       } else {
-        System.out.println("else");
-        Edge tmpEdge = mf.new Edge(b, capacity);
-        grannMap.get(a).add(tmpEdge);
-        residualGraf.get(a).add(tmpEdge);
-        tmpEdge = mf.new Edge(a, capacity);
-        grannMap.get(b).add(tmpEdge);
-        residualGraf.get(b).add(tmpEdge);
+        grannMap.get(from).add(forward);
+        residualGraf.get(from).add(forward);
+        if (!grannMap.containsKey(to)) {
+          ArrayList<Edge> edgeTo = new ArrayList<Edge>();
+          ArrayList<Edge> edgeToR = new ArrayList<Edge>();
+          edgeTo.add(backward);
+          edgeToR.add(backward);
+          grannMap.put(to, edgeTo);
+          residualGraf.put(to, edgeToR);
+        } else {
+          grannMap.get(to).add(backward);
+          residualGraf.get(to).add(backward);
+        }
       }
     }
     // här börjar fordfulkersson
-    ArrayList<Integer> parentList = new ArrayList<Integer>(Collections.nCopies(v + 1,Integer.MAX_VALUE));
+    ArrayList<Edge> parents = new ArrayList<Edge>();
     int maxFlow = 0;
-    System.out.println("calling bfs");
-    while (mf.bfs(residualGraf, s, t, parentList, v)) {
-      System.out.println("rad 73");
+    while (mf.bfs(residualGraf, s, t, parents, v)) {
       int pathFlow = Integer.MAX_VALUE;
-      for (int node = t; node != s; node = parentList.get(node)) {
-        System.out.println("rad 76");
-        int u = parentList.get(node);
-        pathFlow = Math.min(pathFlow, residualGraf.get(u).get(node).capacity);
+      for(Edge edge : parents){
+        if(edge.flow > 0){
+          pathFlow = Math.min(pathFlow, edge.capacity - edge.flow);
+        }
+        else{
+          continue;
+        }
       }
-      for (int node = t; node != s; node = parentList.get(node)) {
-        System.out.println("rad 81");
-        int u = parentList.get(node);
-        residualGraf.get(u).get(node).flow = residualGraf.get(u).get(node).capacity - pathFlow;
-        residualGraf.get(node).get(u).flow = residualGraf.get(node).get(u).capacity + pathFlow;
+      for(Edge edge : parents){
+        edge.flow += pathFlow;
+        edge.reverseEdge.capacity = edge.reverseEdge.capacity - edge.flow;
+
       }
+      maxFlow += pathFlow;
     }
     io.close();
   }
+
   // residualgraf,Edge s, Edge t, parentList<Edges>
-  boolean bfs(HashMap<Integer, ArrayList<Edge>> residualGraf, int source, int sink, ArrayList<Integer> parentList, int v) {
-    System.out.println("in bfs");
-    ArrayList<Boolean> visited = new ArrayList<Boolean>(Collections.nCopies(v + 1,Boolean.FALSE));
+  boolean bfs(HashMap<Integer, ArrayList<Edge>> residualGraf, int source, int sink, ArrayList<Edge> parents, int v) {
+    boolean[] visited = new boolean[v];
+    Arrays.fill(visited, false);
     LinkedList<Integer> queue = new LinkedList<Integer>();
 
     queue.add(source);
-    visited.set(source, true);
-    parentList.set(source, -1);
+    visited[source] = true;
+    parents.add(null);
+
     while (queue.size() != 0) {
-      System.out.println("in while");
-      int pop = queue.poll();
-      for (int i = 0; i <= v; i++) {
-        if(visited.get(i) == false ){
-          if (residualGraf.get(pop).get(i).capacity > 0) {
-            queue.add(i);
-            parentList.set(i, pop);
-            visited.set(i, true);
-          }
+      int currentNode = queue.poll();
+      if (currentNode == sink) {
+        return false;
+      }
+      for (Edge edge : residualGraf.get(currentNode)) {
+        if (visited[edge.to] = false) {
+          visited[edge.to] = true;
+          parents.add(edge);
+          queue.add(edge.to);
         }
-      
       }
     }
-    // Om vi når sink från källa return true, else false
-    return (visited.get(sink));
+    return true;
   }
 }
