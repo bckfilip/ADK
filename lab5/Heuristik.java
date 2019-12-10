5b15af7a3a4f03c664b74d754137cbe26496914b
Filip Bäck <filiplarsback@gmail.com>
2019-12-10 15:11:59 +0100

    easy money

--- lab5/Heuristik.java
+++ lab5/Heuristik.java
@@ -1,19 +1,18 @@
 import java.util.*;
 
 public class Heuristik {
-
 	/**
 	 * Input: V - roller E - scener M - Skådespelare Villkor typ 1 - Möjliga
 	 * skådespelare för roll Villkor typ 2 - Roller i scen
 	 */
 	static Kattio io;
 	static int v, e, m, ss;
-	static HashMap<Integer, ArrayList<Integer>> actorRoles;
-	static HashMap<Integer, ArrayList<Integer>> roleScenes;
-	static HashMap<Integer, ArrayList<Integer>> roleActors;
-	static HashMap<Integer, ArrayList<Integer>> sceneRoles;
+	static HashMap<Integer, Set<Integer>> actorRoles;
+	static HashMap<Integer, Set<Integer>> roleScenes;
+	static HashMap<Integer, Set<Integer>> roleActors;
+	static HashMap<Integer, Set<Integer>> sceneRoles;
 	// Fyll med angivna roller. Key=skådespelare, value=roller.
-	static HashMap<Integer, ArrayList<Integer>> setActorRoles;
+	static HashMap<Integer, Set<Integer>> setActorRoles;
 	// True om rollen är satt.
 	static boolean[] roleIsSet;
 
@@ -33,24 +32,24 @@ public class Heuristik {
 
 		// Fyller maps med listor
 		for (int i = 1; i <= v; i++) {
-			roleScenes.put(i, new ArrayList<Integer>());
-			roleActors.put(i, new ArrayList<Integer>());
+			roleScenes.put(i, new HashSet<Integer>());
+			roleActors.put(i, new HashSet<Integer>());
 		}
 		for (int i = 1; i <= e; i++) {
-			sceneRoles.put(i, new ArrayList<Integer>());
+			sceneRoles.put(i, new HashSet<Integer>());
 		}
 		for (int i = 1; i <= m; i++) {
-			actorRoles.put(i, new ArrayList<Integer>());
-			setActorRoles.put(i, new ArrayList<Integer>());
+			actorRoles.put(i, new HashSet<Integer>());
+			setActorRoles.put(i, new HashSet<Integer>());
 		}
 		// Villkor av typ 1: Vilka skådisar som kan spela varje roll
 		for (int i = 1; i <= v; i++) {
 			int amount = io.getInt();
 			for (int j = 1; j <= amount; j++) {
 				int skådis = io.getInt();
-				ArrayList<Integer> actors = roleActors.get(i);
+				Set<Integer> actors = roleActors.get(i);
 				actors.add(skådis);
-				ArrayList<Integer> roles = actorRoles.get(skådis);
+				Set<Integer> roles = actorRoles.get(skådis);
 				roles.add(i);
 			}
 		}
@@ -59,9 +58,9 @@ public class Heuristik {
 			int amount = io.getInt();
 			for (int j = 1; j <= amount; j++) {
 				int roll = io.getInt();
-				ArrayList<Integer> roles = sceneRoles.get(i);
+				Set<Integer> roles = sceneRoles.get(i);
 				roles.add(roll);
-				ArrayList<Integer> scenes = roleScenes.get(roll);
+				Set<Integer> scenes = roleScenes.get(roll);
 				scenes.add(i);
 			}
 		}
@@ -70,100 +69,94 @@ public class Heuristik {
 		 * återanvända Annars placera ut ny annars superskådis
 		 */
 		setDivas();
-		boolean found = false;
-		boolean found2 = false;
 		for (int roll = 1; roll <= v; roll++) {
+			if (roleIsSet[roll])
+				continue;
 
+			boolean found = false;
+			boolean found2 = false;	
 			// Kolla om vi kan återanvända. actor=1 pga key
-			for (int actor = 1; actor <= setActorRoles.size(); actor++) {
+			for (int actor = 1; actor <= m; actor++) {
 				if (canPlayRole(actor, roll)) {
-					ArrayList<Integer> role = setActorRoles.get(actor);
+					Set<Integer> role = setActorRoles.get(actor);
 					role.add(roll);
 					roleIsSet[roll] = true;
 					found = true;
 					break;
 				}
-				
 			}
-			
 			// Kolla bland icke-tilldelade skådespelare. actor=0 pga value
 			if(!found){
-				ArrayList<Integer> actors = roleActors.get(roll);
-				for (int actor = 0; actor < actors.size(); actor++) {
-				int skådis = actors.get(actor);
-				if (canPlayRole(skådis, roll)) {
-					ArrayList<Integer> role = setActorRoles.get(skådis);
-					role.add(roll);
-					roleIsSet[roll] = true;
-					found2 = true;
-					break;
+				Set<Integer> actors = roleActors.get(roll);
+				for (int skådis : actors) {
+					if (canPlayRole(skådis, roll)) {
+						Set<Integer> role = setActorRoles.get(skådis);
+						role.add(roll);
+						roleIsSet[roll] = true;
+						found2 = true;
+						break;
+					}				
 				}
-				
 			}
-
-			}
-
 			if(!found && !found2){
 				ss++;
-				setActorRoles.put(ss, new ArrayList<Integer>());
-				ArrayList<Integer> roles = setActorRoles.get(ss);
+				setActorRoles.put(ss, new HashSet<Integer>());
+				Set<Integer> roles = setActorRoles.get(ss);
 				roles.add(roll);
 				roleIsSet[roll] = true;
 			}
 		}
-	
-
-	
 	}
 
 	static void setDivas() {
 		int divaOneRole = 0;
 		// Sätt roll för diva1
-		ArrayList<Integer> possibleRoles1 = actorRoles.get(1);
+		Set<Integer> possibleRoles1 = actorRoles.get(1);
 		for (int role1 : possibleRoles1) {
 			divaOneRole = role1;
-			ArrayList<Integer> roles = setActorRoles.get(1);
+			Set<Integer> roles = setActorRoles.get(1);
 			roles.add(role1);
 			roleIsSet[role1] = true;
 			// Sätt roll för diva2
-			ArrayList<Integer> possibleRoles2 = actorRoles.get(2);
+			Set<Integer> possibleRoles2 = actorRoles.get(2);
 			for (int role2 : possibleRoles2) {
 				if (canPlayRole(2, role2)) {
-					ArrayList<Integer> roll = setActorRoles.get(2);
+					Set<Integer> roll = setActorRoles.get(2);
 					roll.add(role2);
 					roleIsSet[role2] = true;
 					return;
 				}
 			}
 			setActorRoles.remove(1);
-			setActorRoles.put(1, new ArrayList<Integer>());
+			setActorRoles.put(1, new HashSet<Integer>());
 			roleIsSet[divaOneRole] = false;
 			divaOneRole = 0;
 		}
 	}
 
 	static boolean canPlayRole(int actor, int role) {
-		if (roleIsSet[role])
+		if (roleIsSet[role] || !actorRoles.get(actor).contains(role))
 			return false;
 
-		for (int scene = 1; scene <= e; scene++) {
+		Set<Integer> scenes = roleScenes.get(role);
+		for(int scene : scenes) {
 			// Specialfall för divor. Om diva redan har roll i denna scen + check om andra
 			// divan spelar i samma scen
 			if (actor == 1 || actor == 2) {
 				int otherDiva = (actor == 1) ? 2 : 1;
-				ArrayList<Integer> roles = setActorRoles.get(otherDiva);
+				Set<Integer> roles = setActorRoles.get(otherDiva);
 				for (int roll : roles) {
-					ArrayList<Integer> scen = sceneRoles.get(scene);
-					if (scen.contains(roll) && scen.contains(role)){
+					Set<Integer> scen = sceneRoles.get(scene);
+					if (scen.contains(roll)){
 						return false;
 					}
 				}
 			}
 			// Om actor redan har roll i denna scen
-			ArrayList<Integer> roles = setActorRoles.get(actor);
+			Set<Integer> roles = setActorRoles.get(actor);
 			for (int roll : roles) {
-				ArrayList<Integer> scen = sceneRoles.get(scene);
-				if (scen.contains(roll) && scen.contains(role)){
+				Set<Integer> rolesInScene = sceneRoles.get(scene);
+				if (rolesInScene.contains(roll)){
 					return false;
 				}
 			}
@@ -172,14 +165,12 @@ public class Heuristik {
 	}
 
 	public static void main(String args[]) {
-
-		
 		cast();
 		StringBuilder str = new StringBuilder();
 		int setActors = 0;
 		int size = setActorRoles.size();
 		for(int i = 1; i <= size; i++){
-			ArrayList<Integer> roles = setActorRoles.get(i);
+			Set<Integer> roles = setActorRoles.get(i);
 			if(roles.size() == 0){
 				continue;
 			}else{
@@ -192,10 +183,6 @@ public class Heuristik {
 			}
 		}
 		System.out.println(setActors);
-		System.out.println(str.toString());
-
-		//io.println(setActors);
-		//io.println(str.toString());
-		
+		System.out.println(str.toString());		
 	}
 }
